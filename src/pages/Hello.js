11421f2ad7c4eaba84fe1d2982be6263eb2225cd
Junlark Sung (a.k.a Kim_Latte) <melodyrepublic4u@gmail.com>
2021-06-09 04:33:40 +0900

    [client/css] build nav, main / visualizing nav, main, login

---
+++ src/pages/Hello.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Hello.css"
+
+function Hello(props) {
+    const {userinfo} = props;
+    if(userinfo) {
+        // 로그인 되어 있을 때 hello페이지를 더이상 보여주지 않고 ShowPage로 리다이렉션한다.
+    } else {
+        return (
+            <div className="greeting">
+                <div className="contentsWrapper">
+                <img id="greeting-logo" src="/resource/seeshow_logo_lite.png" alt="logo" />
+                <div className="subtitle">공연 전시 정보 안내 서비스</div>
+                <div className="title">See SHOW</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Hello;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import axios from "axios";
+import './Login.css'
 
 axios.defaults.withCredentials = true;
 
@@ -42,47 +43,58 @@ class Login extends React.Component {
   };
   render() {
     return (
-      <div>
-        <center>
-          <form onSubmit={(e) => e.preventDefault()}>
-            <div>
-              <span>email</span>
-              <input
-                type="email"
-                onChange={this.handleInputValue("email")}
-              ></input>
-            </div>
-            <div>
-              <span>password</span>
-              <input
-                type="password"
-                onChange={this.handleInputValue("password")}
-              ></input>
-            </div>
-            <button>
-              <Link to="/signup">Sign Up</Link>
-            </button>
-            <button
-              className="btn btn-login"
-              type="submit"
-              onClick={this.handleLogin}
-            >
-              로그인
-            </button>
-            <div>
-              <Link to="/forgotpw">Forgot Password</Link>
-            </div>
-
-            <div>
-              <span>social login</span>
-            </div>
-            
-            {<div className="alert-box">{this.state.errorMessage}</div>}
-          </form>
-        </center>
+      <div className='container'>
+        <div className='window'>
+         <div className='overlay'></div>
+         <div className='content'>
+            <div className='welcome'>Hello There!</div>
+            <div className='input-fields'>
+              <form onSubmit={(e)=> e.preventDefault()}>
+               <input className='input-line full-width' type='email' placeholder='Email' onChange={this.handleInputValue("email")}></input>
+               <input className='input-line full-width' type='password' placeholder='Password' onChange={this.handleInputValue("password")}></input>
+               <div className='alert-box'>{this.state.errorMessage}</div>
+              </form>
       </div>
+      <div className='spacing'>or continue with <span className='highlight'>Facebook</span> / <span className='highlight'>Google</span></div>
+      <div><button className='choicebtn-login' onClick={this.handleLogin}>LOGIN</button></div>
+      <div><button className='choicebtn'>SIGN UP</button></div>
+      <div><button className='choicebtn'>Forgot Password <Link to="/forgotpw"></Link></button></div>
+    </div>
+  </div>
+</div>
+
+
     );
   }
 }
 
 export default withRouter(Login);
+
+
+{/* <div className='login-box'>
+<center>
+  <form onSubmit={(e) => e.preventDefault()}>
+    <div className='email'>
+      <span className='logintitle'>email</span>
+      <input className='form form-email' type="email" onChange={this.handleInputValue("email")}></input>
+    </div>
+    <div className='password'>
+      <span className='logintitle'>password</span>
+      <input className='form form-password' type="password" onChange={this.handleInputValue("password")} ></input>
+    </div>
+    <button className='btn btn-default'> SIGN UP
+      <Link to="/signup"></Link>
+    </button>
+    <button className="btn btn-default" type="submit" onClick={this.handleLogin}>LOGIN</button>
+    <button className='btn btn-default'> Forgot <br/> Password
+      <Link to="/forgotpw"></Link>
+    </button>
+
+    <div className='social'>
+      <span>social login</span>
+    </div>
+    
+    {<div className="alert-box">{this.state.errorMessage}</div>}
+  </form>
+</center>
+</div> */}
